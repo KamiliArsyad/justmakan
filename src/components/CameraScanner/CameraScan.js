29af30ecf26230301d16b850fd426aa1b9d51981
KamiliArsyad <arsyad.ik@gmail.com>
2023-05-14 01:00:08 +0800

    Added dummy toast (click on camera)

--- src/components/CameraScanner/CameraScan.js
+++ src/components/CameraScanner/CameraScan.js
@@ -1,22 +1,37 @@
-import QrScanner from 'react-qr-scanner'
+import QrScanner from "react-qr-scanner";
+import { useToast } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function CameraScanner() {
+  const toast = useToast();
+
+  const toastProps = {
+    title: "Plate Scanned",
+    description: "Your points have been added to your account.",
+    status: "success",
+    duration: 3000,
+    isClosable: true,
+  };
+
   const handleScan = (data) => {
     if (data) {
-      alert(data);
+      toast(toastProps);
     }
-  }
+  };
 
   const handleError = (err) => {
     console.error(err);
-  }
+  };
 
   return (
     <div>
       <QrScanner
         onError={handleError}
         onScan={handleScan}
-        style={{ width: '100%' }}
+        style={{ width: "100%" }}
+        onClick={() => {
+          toast(toastProps);
+        }}
       />
     </div>
   );
